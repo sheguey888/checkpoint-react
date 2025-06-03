b87d09b06aa39b155f22a6c7f64371c37f0cb31f
Cheikh Gueye <sheguey.works@gmail.com>
2025-06-03 18:12:58 +0000

    checkpoint typescript

---
+++ nine-typescript-checkpoint/code-2.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+interface CounterState {
+  count: number;  
+}
+
+class Counter extends Component<{}, CounterState> {
+  state: CounterState = {
+    count: 0
+  };
+
+  increment = (): void => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+export default Counter;
